03995e6d568085a2e46085221f3de0cbbe91ed09
Cut the Wire <102637600+CaDiVae@users.noreply.github.com>
2022-11-30 16:50:20 +0900

    README update

--- 나는야 포켓몬 마스터 이다솜(1620).py
+++ 나는야 포켓몬 마스터 이다솜(1620).py
@@ -1,16 +1,17 @@
-n,m=map(int,input().split())
-P=[]
-quiz_P=[]
-anw_p=[]
-for _ in range(n):
-    P.append(input())
-for _ in range(m):
-    quiz_P.append(input())
-for i in quiz_P:
-    try:
-        if type(int(i)) is int:      
-            anw_p.append(P[int(i)-1])
-    except:
-        anw_p.append(P.index(i)+1)
-for i in anw_p:
-    print(i)
+from sys import stdin
+ip=stdin.readline
+
+n,m=map(int,ip().split())
+P={}
+
+for i in range(1,n+1):
+    p=ip().rstrip()
+    P[i]=p
+    P[p]=i
+
+for i in range(m):
+    quiz_P=ip().rstrip()
+    if quiz_P.isdigit(): #숫자 판별 isdigit()
+        print(P[int(quiz_P)])
+    else:
+        print(P[quiz_P])
