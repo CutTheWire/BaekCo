832ad71a1e81ef27cb3094640a8956c79d8b4a3f
Cut the Wire <102637600+CaDiVae@users.noreply.github.com>
2022-11-30 21:31:16 +0900

    README update

--- 듣보잡.py
+++ 듣보잡.py
@@ -1,10 +1,12 @@
-n,m=map(int,input().split())
+from sys import stdin
+ip=stdin.readline
+n,m=map(int,ip().split())
 n_l=set()
 m_l=set()
 for _ in range(n):
-    n_l.add(input())
+    n_l.add(ip().rstrip())
 for _ in range(m):
-    m_l.add(input())
+    m_l.add(ip().rstrip())
 inter=sorted(list(n_l&m_l))
 print(len(inter))
 print(*inter, sep="\n")
