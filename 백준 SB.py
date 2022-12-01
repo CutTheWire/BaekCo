0fc8f20f6e5514bab41001ae69355cbbc38fade5
Cut the Wire <102637600+CaDiVae@users.noreply.github.com>
2022-12-01 22:51:12 +0900

    README update

--- 백준 SB.py
+++ 백준 SB.py
@@ -4,4 +4,7 @@ def find(s,e):
     m=(s+e)//2;find(s,m);find(m+1,e)
     if K <=e-s+1:J=sorted(I[s:e+1]);print(J[K-1]);exit()
     K -= e-s+1
-N,K=map(int,input().split());I=list(map(int,input().split()));find(0, N-1);print(-1)
+N,K=map(int,input().split())
+I=list(map(int,input().split()))
+find(0, N-1)
+print(-1)
